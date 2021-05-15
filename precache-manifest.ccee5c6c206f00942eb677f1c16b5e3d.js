41eedd6abce4987a786555e139bf07714bb8f6c8
kaveeshwa-iil <kaveeshwa.b@informaticsint.com>
2021-05-15 16:24:38 +0530

    Updates

---
+++ precache-manifest.ccee5c6c206f00942eb677f1c16b5e3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e82879a4a25b5a1a99e0ff87974960",
+    "url": "/tour-guide/index.html"
+  },
+  {
+    "revision": "3fa98cf6532479258da2",
+    "url": "/tour-guide/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/tour-guide/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tour-guide/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa98cf6532479258da2",
+    "url": "/tour-guide/static/js/main.16e880c4.chunk.js"
+  },
+  {
+    "revision": "fb2a22d444ac406a84f3",
+    "url": "/tour-guide/static/js/runtime-main.b3e85f3f.js"
+  }
+]);
